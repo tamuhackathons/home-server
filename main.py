c59ac35440ac92477c18293fa189ff706ce27982
Ruben Vazquez-Chapa <ravc@protonmail.com>
2019-01-26 15:54:01 -0600

    Clean up

--- main.py
+++ main.py
@@ -4,12 +4,24 @@ import configparser
 config = configparser.ConfigParser()
 config.read('main_config.ini')
 
+DEBUG = config['config']['debug']
+HOST = config['config']['host']
+PORT = config['config']['port']
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', port = PORT)
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/<plugin>/')
+def plugins(plugin):
+    main_page = confing[plugin]
+    return 
 
 if __name__ == '__main__':
-    flask_config = config['config']
-    app.run(debug=flask_config['debug'], host=flask_config['host'], port=flask_config['port'])
+    app.run(debug=DEBUG, host=HOST, port=PORT)
