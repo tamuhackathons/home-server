b0afc592fe9121b5f647306a915c180b909d4c50
Ruben Vazquez-Chapa <ravc@protonmail.com>
2019-01-26 18:49:55 -0600

    Fix container creation

--- containers/docker_helper.py
+++ containers/docker_helper.py
@@ -1,7 +1,14 @@
-import docker, configparser
-from plugins.plugin_parser import add_plugin
+import docker, configparser, socket
 
 client = docker.from_env()
+
+def add_plugin(plugin_name, name, url):
+    config.add_section(plugin_name)
+    config.set(plugin_name, "name", name)
+    config.set(plugin_name, "url", url)
+    
+    with open('plugins/plugins_config.ini', 'w') as config_file:
+        config.write(config_file)
 
 def get_free_tcp_port():
     tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,12 +26,18 @@ def current_stopped_containers():
 
 def create_new_plugin(docker_url, name):
     try:
-        bind_port = get_free_tcp_port
+        bind_port = get_free_tcp_port()
         client.containers.run(docker_url, name=name, ports={'80/tcp':str(bind_port)}, detach=True)
         add_plugin(name, name, f'localhost:{bind_port}')
         return 200
-    except:
-        return 400
+    except docker.errors.ContainerError as e:
+        print(e)
+    except docker.errors.ImageNotFound as e:
+        print(e)
+    except docker.errors.APIError as e:
+        print(e)
+        
+    return 400
     
 def stop_container(name):
     for container in current_running_containers():
