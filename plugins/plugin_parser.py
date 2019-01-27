0bf37899368f187e3aa2dd5c6851e000ec73aa63
Ruben Vazquez-Chapa <ravc@protonmail.com>
2019-01-26 20:34:11 -0600

    Add edit

--- plugins/plugin_parser.py
+++ plugins/plugin_parser.py
@@ -12,16 +12,30 @@ def create_dictionary():
     
     return plugins
 
-def add_plugin(plugin_name, name, url):
+def add_plugin(plugin_name, url):
     config.add_section(plugin_name)
-    config.set(plugin_name, "name", name)
+    config.set(plugin_name, "name", plugin_name)
     config.set(plugin_name, "url", url)
     
     with open('plugins/plugins_config.ini', 'w') as config_file:
         config.write(config_file)
         
 def remove_plugin(plugin_name):
-    pass
+    config.remove_section(plugin_name)
+    
+    with open('plugins/plugins_config.ini', 'w') as config_file:
+        config.write(config_file)
+
+def edit_plugin(plugin_name, new_name):
+    try:
+        url = config[plugin_name]['url']
+        
+        remove_plugin(plugin_name)
+        add_plugin(new_name, url)
+    except:
+        pass
 
 if __name__ == '__main__':
+    print(create_dictionary())
+    edit_plugin('some_plugin', 'test')
     print(create_dictionary())
