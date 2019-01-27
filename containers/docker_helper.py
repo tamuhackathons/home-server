f1a988799ebb0bda2b10c19600f0007b34dae118
Ruben Vazquez-Chapa <ravc@protonmail.com>
2019-01-26 20:48:04 -0600

    Clean up

--- containers/docker_helper.py
+++ containers/docker_helper.py
@@ -1,14 +1,9 @@
-import docker, configparser, socket
+import docker, configparser, socket, sys
+sys.path.append('.')
+from plugins.plugin_parser import add_plugin
 
 client = docker.from_env()
 
-def add_plugin(plugin_name, name, url):
-    config.add_section(plugin_name)
-    config.set(plugin_name, "name", name)
-    config.set(plugin_name, "url", url)
-    
-    with open('plugins/plugins_config.ini', 'w') as config_file:
-        config.write(config_file)
 
 def get_free_tcp_port():
     tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
